6ee50b3cd51e768446d35f2341b82bf2d84a9093
linkinghack <linkinghack@outlook.com>
2019-02-20 14:15:00 +0800

    测试URPCrawler工作是否正常

--- urp_test.go
+++ urp_test.go
@@ -32,7 +32,8 @@ func Test_UrpLogin(t *testing.T) {
 func Test_PassedCourses(t *testing.T) {
 	urp := NewUrpCrawler()
 	client, idx, _ := urp.CreateClientAndLogin("2015005973", "lolipop8974.")
-	urp.GetPassedCourses(client, idx)
+	courses, _ := urp.GetPassedCourses(client, idx)
+	fmt.Println(courses)
 }
 
 func Test_FailedCourses(t *testing.T) {
